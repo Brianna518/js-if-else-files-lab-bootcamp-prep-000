9c7a9826faab998471a50c0a00faf2df1cbfe3a6
Anna Fernandez <bri.ann518@gmail.com>
2017-09-16 23:28:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -12,5 +12,5 @@ var index = require("./index.js")
 if (index.height === 74) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 70, Received: 74")
 }
